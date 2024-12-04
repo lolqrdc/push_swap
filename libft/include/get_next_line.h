8842378836f37eed83c2e019b995e8df71b85af6
Lolqrdc <lola-rdc@outlook.fr>
2024-12-04 16:24:42 +0100

    u

--- libft/include/get_next_line.h
+++ libft/include/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 18:42:34 by loribeir          #+#    #+#             */
-/*   Updated: 2024/12/04 09:42:27 by loribeir         ###   ########.fr       */
+/*   Updated: 2024/12/04 16:24:11 by loribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 50
