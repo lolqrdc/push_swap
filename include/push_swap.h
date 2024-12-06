11ddbf24547024fa74a804df641c321130efd737
Lolqrdc <lola-rdc@outlook.fr>
2024-12-06 19:25:57 +0100

    u

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:47:46 by loribeir          #+#    #+#             */
-/*   Updated: 2024/12/06 19:21:14 by loribeir         ###   ########.fr       */
+/*   Updated: 2024/12/06 19:25:46 by loribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,11 @@ void	rotate(t_stack *stack);
 void	rotate_ra(t_stack *stack_a);
 void	rotate_rb(t_stack *stack_b);
 void	rotate_rr(t_stack *stack_a, t_stack *stack_b);
+//
+void	reverse(t_stack *stack_a, t_stack *stack_b);
+void	reverse_rra(t_stack *stack_a);
+void	reverse_rrb(t_stack *stack_b);
+void	reverse_rrr(t_stack *stack_a, t_stack stack_b);
 
 // test
 int 	main(int argc, char **argv);
--- srcs/instructions/reverse.c
+++ srcs/instructions/reverse.c
@@ -6,13 +6,21 @@
 /*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 19:21:51 by loribeir          #+#    #+#             */
-/*   Updated: 2024/12/06 19:22:16 by loribeir         ###   ########.fr       */
+/*   Updated: 2024/12/06 19:25:15 by loribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	reverse()
+void	reverse(t_stack *stack_a, t_stack *stack_b)
 {
 	
 }
+void	reverse_rra(t_stack *stack_a)
+{
+}
+void	reverse_rrb(t_stack *stack_b)
+{
+}
+void	reverse_rrr(t_stack *stack_a, t_stack stack_b)
+{}
